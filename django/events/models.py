86c970a95339e4b2c44a662e2b8f85a0fc303af6
OZoneGuy <alkershomar@protonmail.com>
2020-08-26 17:17:50 -0400

    feat - Added Event model

--- django/events/models.py
+++ django/events/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField()
+    short_desc = models.CharField()
+    long_desc = models.TextField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    banner = models.FileField(upload_to="events/%Y/%m")
