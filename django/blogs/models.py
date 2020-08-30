0fed594150efdaca0bbd271e5c1d30c978626a90
OZoneGuy <alkershomar@protonmail.com>
2020-08-29 22:08:46 -0400

    fix - Fixed models

--- django/blogs/models.py
+++ django/blogs/models.py
@@ -4,8 +4,8 @@ from django.db import models
 # Blog class
 # A class responsible for storing blogs/newsletters
 class Blog(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=100)
     body = models.TextField()
     pub_date = models.DateField(auto_now_add=True)
     last_edit_date = models.DateField(auto_now=True)
-    author = models.CharField()
+    author = models.CharField(max_length=50)
--- django/events/models.py
+++ django/events/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Event(models.Model):
-    title = models.CharField()
-    short_desc = models.CharField()
+    title = models.CharField(max_length=100)
+    short_desc = models.CharField(max_length=500)
     long_desc = models.TextField()
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
