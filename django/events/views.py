35fbeeb022b2d70d2a6b9ee702868e3ab668e498
O-Zone-Guy <alkershomar@gmail.com>
2020-08-31 14:57:12 -0400

    feat - Added events pages

--- django/events/views.py
+++ django/events/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from .models import Event
 
 
 # Create your views here.
 def index(request):
-    return render(request, "events/index.html")
+    context = {
+        'event': Event.objects.all()
+    }
+    return render(request, "events/index.html", context=context)
+
+
+def details(request, id):
+    context = {
+        'event': Event.objects.get(pk=id)
+    }
+    return render(request, "events/details.html", context=context)
