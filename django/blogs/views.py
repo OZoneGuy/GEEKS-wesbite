45152031067b729e8b49981e2da0c28896a80af1
OZoneGuy <alkershomar@protonmail.com>
2020-08-31 14:57:27 -0400

    feat - Added blogs pages

--- django/blogs/views.py
+++ django/blogs/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from .models import Blog
 
 
 # Create your views here.
 def index(request):
-    return render(request, "blogs/index.html")
+    context = {
+        'blog': Blog.objects.all()
+    }
+    return render(request, "blogs/index.html", context=context)
+
+
+def details(request, id):
+    context = {
+        'blog': Blog.objects.get(pk=id)
+    }
+    return render(request, "blogs/details.html", context=context)
