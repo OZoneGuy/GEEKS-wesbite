4b7ac8478b4f02f03c71fbc559895ad452dc5527
O-Zone-Guy <alkershomar@gmail.com>
2020-09-06 22:51:38 -0400

    feat - Created Registration Form

--- django/people/models.py
+++ django/people/models.py
@@ -1,4 +1,6 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+from django import forms
 from django.contrib.auth.models import User as U
 
 
@@ -7,3 +9,35 @@ class User(models.Model):
     user = models.OneToOneField(U,
                                 on_delete=models.CASCADE,
                                 primary_key=True)
+
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(max_length=50,
+                               label="Username")
+    f_name = forms.CharField(max_length=50,
+                             label="First Name")
+    l_name = forms.charField(max_length=50,
+                             label="Last Name")
+    email = forms.EmailField(label="McMaster Email")
+    password = forms.CharField(min_length=8,
+                               widget=forms.PasswordWidget,
+                               label="Password")
+    rep_password = forms.CharField(min_length=8,
+                                   widget=forms.PasswordWidget,
+                                   label="Repeat Password")
+
+    def clean_email(self):
+        domain = self.cleaned_data['email'].split('@')[-1]
+        if (domain.lower() != "mcmaster.ca"):
+            raise ValidationError(
+                "Invalid email. Please use your McMaster email.")
+        return self.cleaned_data['email']
+
+    def clean(self):
+        cleaned_data = super.clean()
+        password = cleaned_data.get('password')
+        rep_password = cleaned_data.get('rep_password')
+
+        if rep_password != password:
+            self.add_error('rep_password', ValidationError(
+                "Passwords do not match."))
