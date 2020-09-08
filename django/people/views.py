cc2fc6d541def0dc6a940c9b863e6eaebf741e62
O-Zone-Guy <alkershomar@gmail.com>
2020-09-08 15:38:04 -0400

    feat - Added membership codes to register view

--- django/people/views.py
+++ django/people/views.py
@@ -1,3 +1,6 @@
+import uuid
+from datetime import date
+
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as l_in
 from django.contrib.auth import logout as l_out
@@ -50,15 +53,34 @@ def register(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             data = form.cleaned_data
-            user = User(user=U.objects.create_user(
-                username=data.get('username'),
-                first_name=data.get('f_name'),
-                last_name=data.get('l_name'),
-                email=data.get('email'),
-                password=data.get('password'),
-                is_staff=False,
-                is_superuser=False,
-                is_active=False,))
+            is_member: bool
+            mem_uuid: uuid
+            if date.today() < date(year=2020, month=10, day=1):
+                is_member = True
+                mem_uuid = uuid.uuid4()
+
+                user = User(user=U.objects.create_user(
+                    username=data.get('username'),
+                    first_name=data.get('f_name'),
+                    last_name=data.get('l_name'),
+                    email=data.get('email'),
+                    password=data.get('password'),
+                    is_staff=False,
+                    is_superuser=False,
+                    is_active=False,),
+                            is_member=is_member,
+                            member_code=mem_uuid)
+            else:
+                user = User(user=U.objects.create_user(
+                    username=data.get('username'),
+                    first_name=data.get('f_name'),
+                    last_name=data.get('l_name'),
+                    email=data.get('email'),
+                    password=data.get('password'),
+                    is_staff=False,
+                    is_superuser=False,
+                    is_active=False))
+
             l_in(request, user.user)
             return redirect('people:register')
 
