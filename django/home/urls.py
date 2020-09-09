eec5df1d78d68ad715005c04844fcb9fda912c84
OZoneGuy <alkershomar@protonmail.com>
2020-09-08 22:44:45 -0400

    fix - Fixed weeklies url

--- django/home/urls.py
+++ django/home/urls.py
@@ -7,5 +7,5 @@ app_name = 'home'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about_us', views.about_us, name='about_us'),
-    path('weeklies', views.about_us, name='weeklies'),
+    path('weeklies', views.weeklies, name='weeklies'),
 ]
