451cbe3082837954b7424ac8a7766de432f3d3ce
OZoneGuy <alkershomar@protonmail.com>
2020-09-08 22:13:39 -0400

    feat - Added about us and weeklies pages

--- django/home/urls.py
+++ django/home/urls.py
@@ -6,4 +6,6 @@ app_name = 'home'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about_us', views.about_us, name='about_us'),
+    path('weeklies', views.about_us, name='weeklies'),
 ]
